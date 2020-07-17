25ea53cb4ea56a021f6dff499de84a5f4b0544b7
EC2 Default User <ec2-user@ip-172-31-2-7.ap-south-1.compute.internal>
2020-07-17 10:45:56 +0000

    poll scm

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
   public static void main(String[] args) {
     System.out.println("Hello, World");
     System.out.println("This is suman class");
+    System.out.println("This is aku alcalss");
+
   }
 }
